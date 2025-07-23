f9bb0bc7217fbaa0eb1aa0a46d8ba5b128c63e16
rahmatramadhanmika <adhanman12@gmail.com>
2025-07-23 14:30:31 +0700

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 import mongoose from "mongoose";
 import app from "./app.js";
 
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost:27017/express-test";
 mongoose
-  .connect("mongodb://localhost:27017/express-test")
+  .connect(mongoUrl)
   .then(() => {
     console.log("database connected");
     app.listen(3000, () => {
